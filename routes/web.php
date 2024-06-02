4d116c25d33b821f767195197d7dc2a8d87dec82
Arnulfo Acosta <arnulfoacosta0887@gmail.com>
2024-06-02 15:49:27 -0500

    se agrega dump a rutas

--- routes/web.php
+++ routes/web.php
@@ -172,6 +172,11 @@ Route::group(['middleware' => ['role:admin']], function () {
     Artisan::call('config:cache');
     return 'Config cache has been cleared';
   });
+
+  Route::get('/dump-autoload', function () {
+    Artisan::call('dump-autoload');
+    return 'dump-autoload complete';
+  });
 });
 
 
